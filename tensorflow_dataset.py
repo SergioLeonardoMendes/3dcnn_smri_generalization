009c54d8f8759e2e3040e08d6c482c578273d1ca
sergioleonardomendes <sergioleonardomendes@gmail.com>
2021-07-30 14:55:00 -0300

    backgroud mask -1's

--- tensorflow_dataset.py
+++ tensorflow_dataset.py
@@ -179,8 +179,8 @@ def parse_tfrecord(serialized_example):
     background_mask4d = tf.cast((fit_background_mask != 1.0), tf.float32)
     image_fitted = image_fitted * background_mask4d
     # get the image background mask and apply a mask composed by -1's
-    # background_mask4d = fit_background_mask() #LEOTMP
-    # image_fitted = image_fitted + (background_mask4d * -1) #LEOTMP
+    background_mask4d = fit_background_mask()
+    image_fitted = image_fitted + (background_mask4d * -1)
 
     # remove csf layer
     image_fitted = image_fitted[:, :, :, :2]
