6cafe1c4b7e9b466f4dedc735cc3999467552430
sergioleonardomendes <sergioleonardomendes@gmail.com>
2021-08-12 12:27:20 -0300

    add DX_ALL feature

--- config/ADHD200/features_transform.py
+++ config/ADHD200/features_transform.py
@@ -6,4 +6,10 @@ def features_transform(example):
     # assign subjectid key
     example['info/subjectid'] = example['info/SCANDIR_ID']
 
+    # create a combined diagnostic of ADHD (including: inattentive, hyperactive and combined)
+    if example['info/DX'] == 0:
+        example['info/DX_ALL'] = 0
+    else:
+        example['info/DX_ALL'] = 1
+
     return example
