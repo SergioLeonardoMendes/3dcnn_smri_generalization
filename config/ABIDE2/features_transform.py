16e50dbfdfb566c8902664cc95150ddb5c68eefb
sergioleonardomendes <sergioleonardomendes@gmail.com>
2021-08-16 16:45:13 -0300

    map DX_GROUP label from 2 to 0

--- config/ABIDE2/features_transform.py
+++ config/ABIDE2/features_transform.py
@@ -6,4 +6,8 @@ def features_transform(example):
     # assign subjectid key
     example['info/subjectid'] = example['info/SUB_ID']
 
+    # map health control (label=2 to label=0) DX_GROUP=2 is autism
+    if example['info/DX_GROUP'] == 2:
+        example['info/DX_GROUP'] = 0
+
     return example
