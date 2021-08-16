8ad0b3048e0c8f9ac218bcf6a26ad3fb360e5f98
sergioleonardomendes <sergioleonardomendes@gmail.com>
2021-08-16 18:32:28 -0300

    add interview_age_years feature

--- config/ABCD/features_transform.py
+++ config/ABCD/features_transform.py
@@ -1,7 +1,12 @@
+import tensorflow as tf
+
 def features_transform(example):
     """ Receive and transform the features from features_dictionary.json"""
 
     # assign subjectid key
     example['info/subjectid'] = example['info/subjectkey']
+
+    # create interview_age_years
+    example['info/interview_age_years'] = tf.cast(example['info/interview_age'], tf.float32) / 12.0
 
     return example
