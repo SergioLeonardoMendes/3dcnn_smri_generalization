bbd972e807b553e3e0d800ba19294f5fafe9f0b6
sergioleonardomendes <sergioleonardomendes@gmail.com>
2021-08-25 11:27:12 -0300

    add execution pipeline control

--- run_experiment.py
+++ run_experiment.py
@@ -7,7 +7,6 @@ from saliency_maps import generate_saliency_maps
 from utils import *
 
 def main(args):
-
     # solve the case where the last character is not a slash
     if args.train_valid_test_path[-1:] != '/':
         args.train_valid_test_path = args.train_valid_test_path + '/'
@@ -24,8 +23,13 @@ def main(args):
     else:
         print("Directory successfully created!\n")
 
-    # remove output_log if it already exists
-    os.remove(args.log_file) if os.path.exists(args.log_file) else None
+    # change output log name if output_log already exists
+    log_count = 0
+    log_file_ori = args.log_file
+    while os.path.exists(args.log_file):
+        log_count += 1
+        args.log_file = log_file_ori.replace('.', '_' + str(log_count) + '.')
+    # os.remove(args.log_file) if os.path.exists(args.log_file) else None
 
     # print configs of the experiment
     print_configs(args)
@@ -64,82 +68,98 @@ def main(args):
             print_file(filename=args.log_file, text='\n  ERROR: train, validation or test csv files do not exist! Exiting...')
             exit(2)
 
-        # predict and collect metric
+        # predict and collect metric if fold output log does not exist
         result_metrics_path = args.out_path + '/results/' + str_current_k + '/'
         os.makedirs(result_metrics_path, exist_ok=True)
         args_fold.log_file = result_metrics_path + 'output.log'
 
-        print_file(filename=args.log_file, text='\n  Predicting training set and collecting metrics:')
-        print_file(filename=args.log_file, text='  ' + result_metrics_path + 'train_dataset.metrics\n')
-        df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.train_csv_path,
-                                           metrics_out_filepath=result_metrics_path + 'train_dataset.metrics')
-        df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_reg.insert(loc=0, column='partition', value='train_set')
-        df_tmp_class.insert(loc=0, column='partition', value='train_set')
-        df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
-        df_metrics_class = df_metrics_class.append(df_tmp_class)
-
-        print_file(filename=args.log_file, text='\n  Predicting validation set and collecting metrics:')
-        print_file(filename=args.log_file, text='  ' + result_metrics_path + 'valid_dataset.metrics\n')
-        df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.valid_csv_path,
-                                           metrics_out_filepath=result_metrics_path + 'valid_dataset.metrics')
-        df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_reg.insert(loc=0, column='partition', value='valid_set')
-        df_tmp_class.insert(loc=0, column='partition', value='valid_set')
-        df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
-        df_metrics_class = df_metrics_class.append(df_tmp_class)
-        # operating pointing selection: save validation cutoff_ops to use in test prediction
-        if 'cutoff_ops' in df_tmp_class:
-            valid_cutoff = df_tmp_class['cutoff_ops'].values
+        if Path(args_fold.log_file).is_file():
+            print_file(filename=args.log_file,
+                       text='\n  WARN: output.log from fold evaluation already exists. Skipping...')
         else:
-            valid_cutoff = None
+            print_file(filename=args.log_file, text='\n  Predicting training set and collecting metrics:')
+            print_file(filename=args.log_file, text='  ' + result_metrics_path + 'train_dataset.metrics\n')
+            df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.train_csv_path,
+                                               metrics_out_filepath=result_metrics_path + 'train_dataset.metrics')
+            df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_reg.insert(loc=0, column='partition', value='train_set')
+            df_tmp_class.insert(loc=0, column='partition', value='train_set')
+            df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
+            df_metrics_class = df_metrics_class.append(df_tmp_class)
 
-        print_file(filename=args.log_file, text='\n  Predicting test set and collecting metrics:')
-        print_file(filename=args.log_file, text='  ' + result_metrics_path + 'test_dataset.metrics\n')
-        df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.test_csv_path,
-                                           metrics_out_filepath=result_metrics_path + 'test_dataset.metrics',
-                                           cutoff_value=valid_cutoff)
-        df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
-        df_tmp_reg.insert(loc=0, column='partition', value='test_set')
-        df_tmp_class.insert(loc=0, column='partition', value='test_set')
-        df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
-        df_metrics_class = df_metrics_class.append(df_tmp_class)
+            print_file(filename=args.log_file, text='\n  Predicting validation set and collecting metrics:')
+            print_file(filename=args.log_file, text='  ' + result_metrics_path + 'valid_dataset.metrics\n')
+            df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.valid_csv_path,
+                                               metrics_out_filepath=result_metrics_path + 'valid_dataset.metrics')
+            df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_reg.insert(loc=0, column='partition', value='valid_set')
+            df_tmp_class.insert(loc=0, column='partition', value='valid_set')
+            df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
+            df_metrics_class = df_metrics_class.append(df_tmp_class)
+            # operating pointing selection: save validation cutoff_ops to use in test prediction
+            if 'cutoff_ops' in df_tmp_class:
+                valid_cutoff = df_tmp_class['cutoff_ops'].values
+            else:
+                valid_cutoff = None
 
-        print_file(filename=args.log_file, text='\n  Generating test set salience and brain maps (SmoothGrad):')
-        saliency_mean, brain_mean = generate_saliency_maps(args_fold, examples_to_map=args_fold.test_csv_path)
-        saliency_list.append(saliency_mean)
-        brain_list.append(brain_mean)
-        print_file(filename=args.log_file, text='  ' + result_metrics_path + 'saliency_mean.npy')
-        np.save(result_metrics_path + 'saliency_mean.npy', saliency_mean)
-        print_file(filename=args.log_file, text='  ' + result_metrics_path + 'brain_mean.npy')
-        np.save(result_metrics_path + 'brain_mean.npy', brain_mean)
+            print_file(filename=args.log_file, text='\n  Predicting test set and collecting metrics:')
+            print_file(filename=args.log_file, text='  ' + result_metrics_path + 'test_dataset.metrics\n')
+            df_tmp_reg, df_tmp_class = predict(args_fold, args_fold.test_csv_path,
+                                               metrics_out_filepath=result_metrics_path + 'test_dataset.metrics',
+                                               cutoff_value=valid_cutoff)
+            df_tmp_reg.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_class.insert(loc=0, column='fold', value=str_current_k)
+            df_tmp_reg.insert(loc=0, column='partition', value='test_set')
+            df_tmp_class.insert(loc=0, column='partition', value='test_set')
+            df_metrics_reg = df_metrics_reg.append(df_tmp_reg)
+            df_metrics_class = df_metrics_class.append(df_tmp_class)
 
-    # clear dataframes' indices
-    blank_idx = [''] * len(df_metrics_reg)
-    df_metrics_reg.index = blank_idx
-    blank_idx = [''] * len(df_metrics_class)
-    df_metrics_class.index = blank_idx
-    # print resultant dataframes
-    print_file(filename=args.log_file, text='\n----- Write metrics to csv files -----')
-    print_file(filename=args.log_file, text='  ' + args.out_path + '/results/metrics_regression.csv')
-    print_file(filename=args.log_file, text='  ' + args.out_path + '/results/metrics_classification.csv')
-    df_metrics_reg.to_csv(args.out_path + '/results/metrics_regression.csv')
-    df_metrics_class.to_csv(args.out_path + '/results/metrics_classification.csv')
-    print_file(filename=args.log_file, text='\n----- Regression Metrics -----')
-    print_file(filename=args.log_file, text=df_metrics_reg.to_string())
-    print_file(filename=args.log_file, text='\n----- Classification Metrics -----')
-    print_file(filename=args.log_file, text=df_metrics_class.to_string())
+        if Path(result_metrics_path + 'saliency_mean.npy').is_file():
+            print_file(filename=args.log_file,
+                       text='\n  WARN: saliency_mean.npy from fold already exists. Skipping...')
+        else:
+            print_file(filename=args.log_file, text='\n  Generating test set salience and brain maps (SmoothGrad):')
+            saliency_mean, brain_mean = generate_saliency_maps(args_fold, examples_to_map=args_fold.test_csv_path)
+            saliency_list.append(saliency_mean)
+            brain_list.append(brain_mean)
+            print_file(filename=args.log_file, text='  ' + result_metrics_path + 'saliency_mean.npy')
+            np.save(result_metrics_path + 'saliency_mean.npy', saliency_mean)
+            print_file(filename=args.log_file, text='  ' + result_metrics_path + 'brain_mean.npy')
+            np.save(result_metrics_path + 'brain_mean.npy', brain_mean)
 
-    print_file(filename=args.log_file, text='\n----- Salience and brain maps (SmoothGrad) -----')
-    saliency_mean = np.mean(np.asarray(saliency_list, dtype=np.float32), axis=0)
-    brain_mean = np.mean(np.asarray(brain_list, dtype=np.float32), axis=0)
-    print_file(filename=args.log_file, text='  ' + args.out_path + '/results/saliency_mean.npy')
-    np.save(args.out_path + '/results/saliency_mean.npy', saliency_mean)
-    print_file(filename=args.log_file, text='  ' + args.out_path + '/results/brain_mean.npy')
-    np.save(args.out_path + '/results/brain_mean.npy', brain_mean)
+    if Path(args.out_path + '/results/metrics_regression.csv').is_file():
+        print_file(filename=args.log_file,
+                   text='\n  WARN: csv table metrics already exists. Skipping... ')
+    else:
+        # clear dataframes' indices
+        blank_idx = [''] * len(df_metrics_reg)
+        df_metrics_reg.index = blank_idx
+        blank_idx = [''] * len(df_metrics_class)
+        df_metrics_class.index = blank_idx
+        # print resultant dataframes
+        print_file(filename=args.log_file, text='\n----- Write metrics to csv files -----')
+        print_file(filename=args.log_file, text='  ' + args.out_path + '/results/metrics_regression.csv')
+        print_file(filename=args.log_file, text='  ' + args.out_path + '/results/metrics_classification.csv')
+        df_metrics_reg.to_csv(args.out_path + '/results/metrics_regression.csv')
+        df_metrics_class.to_csv(args.out_path + '/results/metrics_classification.csv')
+        print_file(filename=args.log_file, text='\n----- Regression Metrics -----')
+        print_file(filename=args.log_file, text=df_metrics_reg.to_string())
+        print_file(filename=args.log_file, text='\n----- Classification Metrics -----')
+        print_file(filename=args.log_file, text=df_metrics_class.to_string())
+
+    if Path(args.out_path + '/results/saliency_mean.npy').is_file():
+        print_file(filename=args.log_file,
+                   text='\n  WARN: mean saliency_mean.npy already exists. Skipping... ')
+    else:
+        print_file(filename=args.log_file, text='\n----- Salience and brain maps (SmoothGrad) -----')
+        saliency_mean = np.mean(np.asarray(saliency_list, dtype=np.float32), axis=0)
+        brain_mean = np.mean(np.asarray(brain_list, dtype=np.float32), axis=0)
+        print_file(filename=args.log_file, text='  ' + args.out_path + '/results/saliency_mean.npy')
+        np.save(args.out_path + '/results/saliency_mean.npy', saliency_mean)
+        print_file(filename=args.log_file, text='  ' + args.out_path + '/results/brain_mean.npy')
+        np.save(args.out_path + '/results/brain_mean.npy', brain_mean)
 
     print_file(filename=args.log_file, text="\n----- DONE! -----")
 
